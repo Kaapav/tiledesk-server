847f4ac61320d75993208743fc7c38de1e75a046
Kaapav <kaapavin@gmail.com>
2025-07-07 11:31:52 +0530

    Update index.js

--- packages/tiledesk-whatsapp-jobworker/index.js
+++ packages/tiledesk-whatsapp-jobworker/index.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://kaapavin:Kaapav@1428@cluster0.usauxbv.mongodb.net/tiledesk?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 20000  // wait up to 20 seconds
+});
+  
   .then(() => console.log("✅ WhatsApp Worker MongoDB Connected"))
   .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
